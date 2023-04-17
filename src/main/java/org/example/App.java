c5227ee9786d21dfd7688c08e6831d4d9e9a2a3f
Mary Me <marymoll202@gmail.com>
2023-04-17 14:29:18 +0300

    hello mates

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -5,8 +5,10 @@ package org.example;
  *
  */
 public class App 
-{ 
+{
     public static void main( String[] args ) {
         System.out.println( "Hello World!" );
+        System.out.println("Hello, mates!");
+
     }
 }
